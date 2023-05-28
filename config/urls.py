7d3b136771bb644771f3d1236e1fc2f8db8681fe
Sevbo <malikov@sevbo.sevbo>
2023-05-29 00:33:27 +0500

    `static` urls setted

--- config/urls.py
+++ config/urls.py
@@ -12,4 +12,7 @@ urlpatterns = [
     path('api/v1/', include('apps.magazine.urls')),
 
 
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] 
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
