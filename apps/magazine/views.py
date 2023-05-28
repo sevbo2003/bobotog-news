6b94483826cada396f2b6f494339ae94d6e59af9
Sevbo <malikov@sevbo.sevbo>
2023-05-29 00:30:12 +0500

    `MagazineViewSet` created

--- apps/magazine/views.py
+++ apps/magazine/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from rest_framework import status
+from apps.magazine.models import Magazine
+from apps.magazine.serializers import MagazineSerializer
 
-# Create your views here.
+
+class MagazineViewSet(viewsets.ModelViewSet):
+    queryset = Magazine.objects.all()
+    serializer_class = MagazineSerializer
+    http_method_names = ['get', 'head', 'options']
+
+    @action(detail=True, methods=['get'])
+    def download(self, request, pk=None):
+        magazine = self.get_object()
+        magazine.downloads_count += 1
+        magazine.save()
+        return Response(status=status.HTTP_200_OK)
+    
