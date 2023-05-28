e0528209646701984bef5f9a2c2c310148a6130d
Sevbo <malikov@sevbo.sevbo>
2023-05-29 00:53:29 +0900

    `PostImageSerializer`

--- apps/blog/serializers.py
+++ apps/blog/serializers.py
@@ -1,9 +1,15 @@
 from rest_framework import serializers
-from apps.blog.models import Category, Post
+from apps.blog.models import Category, Post, PostImage
 
 
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
         fields = ['id', 'name', 'image']
+
+
+class PostImageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = PostImage
+        fields = ['id', 'image', 'video']
         
