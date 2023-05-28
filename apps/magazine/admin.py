f5eb55e0244dff94d8c3d72a444321b0a0c1a990
Sevbo <malikov@sevbo.sevbo>
2023-05-29 04:17:57 +0900

    `MagazineAdmin` admin created

--- apps/magazine/admin.py
+++ apps/magazine/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from apps.magazine.models import Magazine
 
-# Register your models here.
+
+@admin.register(Magazine)
+class MagazineAdmin(admin.ModelAdmin):
+    list_display = ['name', 'created_at', 'hajmi', 'downloads_count']
+    list_filter = ['created_at']
+    search_fields = ['name']
+    readonly_fields = ['hajmi', 'downloads_count']
+    fieldsets = (
+        ('Asosiy ma\'lumotlar', {
+            'fields': ('name', 'file')
+        }),
+        ('Qo\'shimcha ma\'lumotlar', {
+            'fields': ('created_at', 'hajmi', 'downloads_count')
+        })
+    )
+    
