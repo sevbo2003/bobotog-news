0603695668e44f463251457ab7b2ff3ed64ba7ec
Sevbo <malikov@sevbo.sevbo>
2023-06-16 12:35:49 +0500

    `load-dotenv` installed

--- config/config/common.py
+++ config/config/common.py
@@ -311,3 +311,8 @@ class Common(Configuration):
         }
     }
 
+import os
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv(os.path.join(BASE_DIR, '.env'))
