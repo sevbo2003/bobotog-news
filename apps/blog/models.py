d69c771bd8884b5410ee113c7b3f257f94dd9cff
Sevbo <malikov@sevbo.sevbo>
2023-06-29 20:48:29 +0500

    null=True for `author` field inside `Post`

--- apps/blog/models.py
+++ apps/blog/models.py
@@ -36,7 +36,7 @@ class Post(models.Model):
     is_featured = models.BooleanField(default=False, verbose_name=_('Maxus post'))
     slug = models.SlugField(max_length=255, verbose_name=_('Slug'))
     views = models.IntegerField(default=0, verbose_name=_('Ko\'rilganlar soni'))
-    author = models.CharField(max_length=255, verbose_name=_('Muallif'))
+    author = models.CharField(max_length=255, verbose_name=_('Muallif'), null=True, blank=True)
 
     def __str__(self):
         return self.title
